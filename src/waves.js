f687765275e457bf4ea765db8962baceaa7bd6ac
Mighty kitten <81363730+Kirpichic@users.noreply.github.com>
2022-11-08 15:35:25 +0300

    2

--- src/waves.js
+++ src/waves.js
@@ -1,19 +1,19 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
-canvas.header = window.innerHeight;
+canvas.height = window.innerHeight;
 var waves = ["rgba(157, 187, 210, 0.3)"]
 var i = 0;
 function draw(){
     canvas.width = canvas.width;
-    var offset = i + i * Math.PI * 12;
+    var offset = i + 1 * Math.PI * 12;
     ctx.fillStyle = "#138CCB";
     var randomLeft = (Math.sin(offset/100)+1)/2*150;
     var randomRight = (Math.sin((offset/100)+10)+1)/2*150;
-    var randomLeft = (Math.sin((offset/60)+2)+1)/2*150;
-    var randomLeft = (Math.sin((offset/60)+1)+1)/2*150;
+    var randomLeftConstraint = (Math.sin((offset/60)+2)+1)/2*150;
+    var randomRightConstraint = (Math.sin((offset/60)+1)+1)/2*150;
     ctx.beginPath();
-    ctx.maveTo(0, randomLeft + 150);
+    ctx.moveTo(0, randomLeft + 150);
     ctx.bezierCurveTo(canvas.width/3, randomLeftConstraint, canvas.width/3*2, randomRightConstraint, canvas.width, randomRight+150);
     ctx.lineTo(canvas.width, canvas.height);
     ctx.lineTo(0, canvas.height);
